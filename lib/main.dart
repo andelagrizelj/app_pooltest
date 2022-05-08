40e99a02f541c2f952967d7e15a0aef24e9a5d0d
G4bru <to.ahmad.nasir@gmail.com>
2022-05-08 22:15:57 +0200

    Teilnehmer sind nun erstellbar. User daten werden korrekt abgerufen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:app_pooltest/screen/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -45,110 +48,143 @@ class LoginPage extends StatefulWidget {
 class _LoginPage extends State<LoginPage> {
 
   bool _isChecked = false;
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _vornameController = TextEditingController();
+  TextEditingController _geburtsdatumController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _telefonController = TextEditingController();
+  TextEditingController _klasseController = TextEditingController();
+
+  void createUser() async {
+    var url = "http://10.0.2.2:8080/api/createTeilnehmer";
+
+    Map data={
+      'vorname':_vornameController.text,
+      'nachname':_nameController.text,
+      'email':_emailController.text
+    };
+
+    var body = json.encode(data);
+
+    http.Response response = await http.post(Uri.parse(url), headers: {
+      "Content-Type":"application/json"},
+        body:body);
+
+    print(response.body);
+  }
+
 
   @override
   Widget build(BuildContext context) {
     var checkedValue;
     return Center(
-      child: Column(
-        children: <Widget>[
-          Container(
-            margin: const EdgeInsets.all(10.0),
-            color: Colors.white,
-            width: 500.0,
-            height: 20.0,
-            alignment: Alignment.topLeft,
-            child: Text('Geben Sie bitte ihre persönlichen Daten ein',
-                style: Theme
-                    .of(context)
-                    .textTheme
-                    .subtitle1!
-                    .copyWith(color: Colors.black)),
-          ),
-          const SizedBox(height: 20),
-          const TextField(
-            obscureText: false,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Name',
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              color: Colors.white,
+              width: 500.0,
+              height: 20.0,
+              alignment: Alignment.topLeft,
+              child: Text('Geben Sie bitte ihre persönlichen Daten ein',
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .subtitle1!
+                      .copyWith(color: Colors.black)),
             ),
-          ),
-          const SizedBox(height: 10),
-          const TextField(
-            obscureText: false,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Vorname',
-            ),
-          ),
-          const SizedBox(height: 10),
-          const TextField(
-            obscureText: false,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Geburtsdatum',
-            ),
-          ),
-          const SizedBox(height: 10),
-          const TextField(
-            obscureText: false,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'E-Mail Adresse',
-            ),
-          ),
-          const SizedBox(height: 10),
-          const TextField(
-            obscureText: false,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Telefonnummer',
-            ),
-          ),
-          const SizedBox(height: 10),
-          const TextField(
-            obscureText: false,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Klasse',
-            ),
-          ),
-          const SizedBox(height: 10),
-          CheckboxListTile(
-            title: Text("Ich stimme der unter folgendem Link zu findenden Datenschutzerklärung zu: “Datenschutzerklärung.de”"),
-            controlAffinity: ListTileControlAffinity.leading,
-            value: _isChecked,
-            onChanged: (bool? value){
-              setState(() {
-                _isChecked = value!;
-              });
-            },
-          ),
-          const SizedBox(height: 5),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              minimumSize: const Size(300, 50),
-              maximumSize: const Size(300, 50),
-              primary: Colors.lightBlueAccent,
-              textStyle: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 25
+            const SizedBox(height: 20),
+            TextField(
+              controller: _nameController,
+              obscureText: false,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Name',
               ),
             ),
-            //disabeln kann man wenn hier null steht
-            onPressed: (){
-              Navigator.pushAndRemoveUntil(
-                context,
-                PageTransition(
-                    child: Homepage(),
-                    type: PageTransitionType.rightToLeft),
-                    (route) => false,
-              );
-            },
-            child: const Text('Weiter'),
-          ),
-        ],
-      )
+            const SizedBox(height: 10),
+            TextField(
+              controller: _vornameController,
+              obscureText: false,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Vorname',
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _geburtsdatumController,
+              obscureText: false,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Geburtsdatum',
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _emailController,
+              obscureText: false,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'E-Mail Adresse',
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _telefonController,
+              obscureText: false,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Telefonnummer',
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _klasseController,
+              obscureText: false,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Klasse',
+              ),
+            ),
+            const SizedBox(height: 10),
+            CheckboxListTile(
+              title: Text(
+                  "Ich stimme der unter folgendem Link zu findenden Datenschutzerklärung zu: “Datenschutzerklärung.de”"),
+              controlAffinity: ListTileControlAffinity.leading,
+              value: _isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  _isChecked = value!;
+                });
+              },
+            ),
+            const SizedBox(height: 5),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(300, 50),
+                maximumSize: const Size(300, 50),
+                primary: Colors.lightBlueAccent,
+                textStyle: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 25
+                ),
+              ),
+              //disabeln kann man wenn hier null steht
+              onPressed: () {
+                createUser();
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  PageTransition(
+                      child: Homepage(),
+                      type: PageTransitionType.rightToLeft),
+                      (route) => false,
+                );
+              },
+              child: const Text('Weiter'),
+            ),
+          ],
+        )
     );
   }
 }
--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,148 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
 
   static const String _title = 'PCR Pooltest App';
 
+
   @override
   State<Homepage> createState() => _Homepage();
 }
 
 class _Homepage extends State<Homepage> {
+
+  var _userDaten;
+  void getData() async{
+    var url="http://10.0.2.2:8080/api/getUser/1";
+
+    http.Response response = await http.get(Uri.parse(url),headers: {
+      "Accept":"application/json"
+    });
+
+    Map data = json.decode(response.body);
+    print(data["vorname"]);
+
+    setState(() {
+      _userDaten=data;
+    });
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
 
-    //final ButtonStyle style =
-    //ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 10));
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Willkommen XY"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          //mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.all(10.0),
-              color: Colors.white,
-              width: 300.0,
-              height: 200.0,
-              alignment: Alignment.topLeft,
-              child: Text('Hallo XY, \nWas möchtest du tun?',
-                  style: Theme
-                      .of(context)
-                      .textTheme
-                      .headline5!
-                      .copyWith(color: Colors.black)),
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(300, 70),
-                maximumSize: const Size(300, 70),
-                primary: Colors.lightBlueAccent,
-                textStyle: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 25
-                ),
-              ),
-              //disabeln kann man wenn hier null steht
-              onPressed: () {},
-              child: const Text('Termin buchen'),
-            ),
-            const SizedBox(height: 30),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(300, 70),
-                maximumSize: const Size(300, 70),
-                primary: Colors.lightBlueAccent,
-                textStyle: const TextStyle(
-                  fontSize: 25,
-                ),
-              ),
-              onPressed: () {},
-              child: const Text('Termin einsehen'),
-            ),
-            const SizedBox(height: 30),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(300, 70),
-                maximumSize: const Size(300, 70),
-                primary: Colors.lightBlueAccent,
-                textStyle: const TextStyle(
-                  fontSize: 25,
-                ),
-              ),
-              onPressed: () {},
-              child: const Text('persönliche Daten einsehen'),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text("Willkommen "+_userDaten["vorname"]),
         ),
-      )
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.all(10.0),
+                color: Colors.white,
+                width: 300.0,
+                height: 200.0,
+                alignment: Alignment.topLeft,
+                child: Text("Willkommen "+_userDaten["vorname"]+"!"+"\nWas möchtest du tun?",
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .headline5!
+                        .copyWith(color: Colors.black)),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(300, 70),
+                  maximumSize: const Size(300, 70),
+                  primary: Colors.lightBlueAccent,
+                  textStyle: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 25
+                  ),
+                ),
+                //disabeln kann man wenn hier null steht
+                onPressed: () {},
+                child: const Text('Termin buchen'),
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(300, 70),
+                  maximumSize: const Size(300, 70),
+                  primary: Colors.lightBlueAccent,
+                  textStyle: const TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text('Termin einsehen'),
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(300, 70),
+                  maximumSize: const Size(300, 70),
+                  primary: Colors.lightBlueAccent,
+                  textStyle: const TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text('persönliche Daten einsehen'),
+              ),
+            ],
+          ),
+        )
     );
-    /*
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        //mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            margin: const EdgeInsets.all(10.0),
-            color: Colors.white,
-            width: 300.0,
-            height: 200.0,
-            alignment: Alignment.topLeft,
-            child: Text('Hallo XY, \nWas möchtest du tun?',
-                style: Theme
-                    .of(context)
-                    .textTheme
-                    .headline5!
-                    .copyWith(color: Colors.black)),
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              minimumSize: const Size(300, 70),
-              maximumSize: const Size(300, 70),
-              primary: Colors.lightBlueAccent,
-              textStyle: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 25
-              ),
-            ),
-            //disabeln kann man wenn hier null steht
-            onPressed: () {},
-            child: const Text('Termin buchen'),
-          ),
-          const SizedBox(height: 30),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              minimumSize: const Size(300, 70),
-              maximumSize: const Size(300, 70),
-              primary: Colors.lightBlueAccent,
-              textStyle: const TextStyle(
-                fontSize: 25,
-              ),
-            ),
-            onPressed: () {},
-            child: const Text('Termin einsehen'),
-          ),
-          const SizedBox(height: 30),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              minimumSize: const Size(300, 70),
-              maximumSize: const Size(300, 70),
-              primary: Colors.lightBlueAccent,
-              textStyle: const TextStyle(
-                fontSize: 25,
-              ),
-            ),
-            onPressed: () {},
-            child: const Text('persönliche Daten einsehen'),
-          ),
-        ],
-      ),
-    );*/
   }
 }
