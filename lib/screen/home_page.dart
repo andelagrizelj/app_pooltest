c331d577026c21ee770e23ff56efe548ade9ecf1
G4bru <to.ahmad.nasir@gmail.com>
2022-05-08 14:10:33 +0200

    Homepage wird nun mit korrektem Design geladen

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -16,6 +16,73 @@ class _Homepage extends State<Homepage> {
     //final ButtonStyle style =
     //ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 10));
 
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Willkommen XY"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              color: Colors.white,
+              width: 300.0,
+              height: 200.0,
+              alignment: Alignment.topLeft,
+              child: Text('Hallo XY, \nWas möchtest du tun?',
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .headline5!
+                      .copyWith(color: Colors.black)),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(300, 70),
+                maximumSize: const Size(300, 70),
+                primary: Colors.lightBlueAccent,
+                textStyle: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 25
+                ),
+              ),
+              //disabeln kann man wenn hier null steht
+              onPressed: () {},
+              child: const Text('Termin buchen'),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(300, 70),
+                maximumSize: const Size(300, 70),
+                primary: Colors.lightBlueAccent,
+                textStyle: const TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+              onPressed: () {},
+              child: const Text('Termin einsehen'),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(300, 70),
+                maximumSize: const Size(300, 70),
+                primary: Colors.lightBlueAccent,
+                textStyle: const TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+              onPressed: () {},
+              child: const Text('persönliche Daten einsehen'),
+            ),
+          ],
+        ),
+      )
+    );
+    /*
     return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
@@ -76,6 +143,6 @@ class _Homepage extends State<Homepage> {
           ),
         ],
       ),
-    );
+    );*/
   }
 }
